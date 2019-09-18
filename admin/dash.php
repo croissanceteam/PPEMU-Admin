2044623278d878a788aa8254fc651e99a1ad2e03
LamaDev <hlama@live.fr>
2019-09-18 08:16:52 +0100

    Fix Ranking DataTable

--- admin/dash.php
+++ admin/dash.php
@@ -5,13 +5,18 @@
     }
     include_once 'Metier/Autoloader.php';
     Autoloader::register();
+    
 ?>
+ <?php
+     $rapport = new RapportOperation();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>PPEMU | Dashboard</title>
+  <title>PEMU | Tableau de bord</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -20,15 +25,13 @@
   <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
-  <!-- jvectormap -->
-  <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-  <!-- Alertify -->
-  <link rel="stylesheet" href="vendor/alertify/themes/alertify.css" />
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -43,12 +46,10 @@
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
-
- <?php include_once 'partials/header.php' ?>
+<?php include_once 'partials/header.php' ?>
   <!-- Left side column. contains the logo and sidebar -->
   <aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
-    <section class="sidebar">
+  <section class="sidebar">
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
@@ -119,7 +120,6 @@
         <?php endif ?>
       </ul>
     </section>
-    <!-- /.sidebar -->
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
@@ -135,11 +135,9 @@
         <li class="active">Tableau de bord</li>
       </ol>
     </section>
-
     <!-- Main content -->
     <section class="content">
-      <!-- Info boxes -->
-      <div class="row">
+    <div class="row">
         <div class="col-md-3 col-sm-6 col-xs-12">
           <div class="info-box">
             <span class="info-box-icon bg-aqua"><i class="fa fa-cloud-download"></i></span>
@@ -154,7 +152,6 @@
           </div>
           <!-- /.info-box -->
         </div>
-        <!-- /.col -->
         <div class="col-md-3 col-sm-6 col-xs-12">
           <div class="info-box">
             <span class="info-box-icon bg-red"><i class="fa fa-check-square-o"></i></span>
@@ -203,38 +200,26 @@
         </div>
         <!-- /.col -->
       </div>
-      <!-- /.row -->
-      
-        <?php
-            $rapport = new RapportOperation();
-        ?>
-
       <div class="row">
-        <div class="col-md-12">
-          <div class="box">
-            <div class="box-header with-border">
-              <h3 class="box-title">Résumé des Opérations</h3>
+        <div class="col-xs-12">
 
-              <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-              </div>
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Résumé des Opérations</h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-              <div class="row">
-                <div class="col-md-12">
-                    <div class="box-body table-responsive no-padding">
-                      <table class="table table-hover">
-                        <tr>
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
                           <th>#</th>
                           <th>Opérations</th>
                           <th>Description</th>
                           <th>Date</th>
-                        
-                        </tr>
-                        <?php
+                </tr>
+                </thead>
+                <tbody>
+                <?php
                             $resData=$rapport->getJournales(); 
     
                             if ($resData) {
@@ -255,36 +240,31 @@
                         <?php 
                                 }
                             }
-                            else echo "<tr><td colspan='5'><h3 style='color:#d44d06'>Aucune données dans le Journal</h3></td></tr>"
-                        ?>
                         
-                      </table>
-                    </div>
-                </div>
-                <!-- /.col -->
-              </div>
-              <!-- /.row -->
+                        ?>
+                </tbody>
+                <tfoot>
+                <tr>
+                          <th>#</th>
+                          <th>Opérations</th>
+                          <th>Description</th>
+                          <th>Date</th>
+                </tr>
+                </tfoot>
+              </table>
             </div>
-            <!-- ./box-body -->
+            <!-- /.box-body -->
           </div>
           <!-- /.box -->
         </div>
         <!-- /.col -->
       </div>
-      
       <!-- /.row -->
     </section>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-
   <?php include_once 'partials/footer.php' ?>
-
-  <!-- /.control-sidebar -->
-  <!-- Add the sidebar's background. This div must be placed
-       immediately after the control sidebar -->
-  <div class="control-sidebar-bg"></div>
-
 </div>
 <!-- ./wrapper -->
 
@@ -292,26 +272,30 @@
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
 <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- DataTables -->
+<script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
 <script src="bower_components/fastclick/lib/fastclick.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
-<!-- Sparkline -->
-<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<!-- jvectormap  -->
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- SlimScroll -->
-<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- ChartJS -->
-<script src="bower_components/chart.js/Chart.js"></script>
-<!-- alertify -->
-<script src="vendor/alertify/lib/alertify.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard2.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/chscript.js"></script>
+<!-- page script -->
+<script>
+  $(function () {
+    $('#example1').DataTable()
+    $('#example2').DataTable({
+      'paging'      : true,
+      'lengthChange': false,
+      'searching'   : false,
+      'ordering'    : true,
+      'info'        : true,
+      'autoWidth'   : false
+    })
+  })
+</script>
 </body>
 </html>
